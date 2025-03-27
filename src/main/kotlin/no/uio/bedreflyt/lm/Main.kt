dcd36b6c6105ee42ecaa005e967da064235d370c
Riccardo Sieve <sievericcardo@gmail.com>
2025-03-27 14:41:53 +0100

    Removed jpa

--- src/main/kotlin/no/uio/bedreflyt/lm/Main.kt
+++ src/main/kotlin/no/uio/bedreflyt/lm/Main.kt
@@ -3,7 +3,6 @@ package no.uio.bedreflyt.lm
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["no.uio.bedreflyt.lm.config"])
