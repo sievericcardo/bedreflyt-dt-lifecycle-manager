585682ff78619839e6edf2f800124990e49fa0c0
Riccardo Sieve <sievericcardo@gmail.com>
2025-06-17 15:17:01 +0200

    Added opening office

--- src/main/kotlin/no/uio/bedreflyt/lm/service/OfficeService.kt
+++ src/main/kotlin/no/uio/bedreflyt/lm/service/OfficeService.kt
@@ -7,6 +7,7 @@ import no.uio.bedreflyt.lm.types.TreatmentRoom
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URI
 
@@ -15,6 +16,37 @@ class OfficeService {
 
     private val log: Logger = LoggerFactory.getLogger(OfficeService::class.java.name)
     private val objectMapper = jacksonObjectMapper()
+
+    fun createOffice(endpoint: String, hospitalCode: String, wardName: String, capacity: Int, roomNumber: Int, penalty: Double): Boolean {
+        val roomConnection = URI(endpoint).toURL().openConnection() as HttpURLConnection
+        roomConnection.requestMethod = "POST"
+        roomConnection.setRequestProperty("Content-Type", "application/json")
+        roomConnection.doOutput = true
+        val request = mapOf(
+            "roomNumber" to roomNumber,
+            "capacity" to capacity,
+            "penalty" to penalty,
+            "hospital" to hospitalCode,
+            "ward" to wardName,
+            "categoryDescription" to "Midlertidig"
+        )
+
+        val jsonBody = jacksonObjectMapper().writeValueAsString(request)
+
+        log.info("Invoking API with request $jsonBody")
+        OutputStreamWriter(roomConnection.outputStream).use {
+            it.write(jsonBody)
+        }
+
+        if (roomConnection.responseCode == 200) {
+            log.info("API returned status code ${roomConnection.responseCode}")
+            return true
+        } else {
+            log.warn("API returned status code ${roomConnection.responseCode}")
+        }
+
+        return false
+    }
 
     fun retrieveOffices(endpoint: String): List<Office> {
         val officeConnection = URI(endpoint).toURL().openConnection() as HttpURLConnection
